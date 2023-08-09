141d27037ac4e5c5b92f795e9f2ade9dee886c53
hellomaker <xianzhikun@gmail.com>
2023-08-09 22:55:11 +0800

    第一次push

---
+++ wechat/src/test/java/com/hellomaker/wechat/WechatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hellomaker.wechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WechatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
